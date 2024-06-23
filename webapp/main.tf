73d8b058a74b126b096168ff89ae6064ffde4fc2
chenna0501 <chennakesavulu0501@gmail.com>
2024-06-23 21:01:07 +0530

    dfs changes

--- webapp/main.tf
+++ webapp/main.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "capesrgp" {
   name     = "capes-rgp"
   location = "North Europe"
 }
+
+resource "azurerm_resource_group" "dfsrgp" {
+  name     = "dfs-rgp"
+  location = "North Europe"
+}
